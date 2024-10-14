3bd02701b0f51c01ac66557c3d1c152b01eb51aa
Segueregis <105684859+Segueregis@users.noreply.github.com>
2024-10-13 21:04:12 -0300

    Update app.py
    ATUALIZAÇÃO, INCLUINDO FILTRO NOS GRAFICOS

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from dash import dcc, html, Dash
+from dash import dcc, html, Dash, Input, Output
 import dash_bootstrap_components as dbc
 import plotly.graph_objects as go
 from dash_bootstrap_templates import ThemeSwitchAIO
@@ -13,29 +13,32 @@ df_excel = pd.read_excel('Controle_orcamento.xlsx', sheet_name='Base')
 # Exemplo de Orçamento Fixo para cada UT e Categoria
 data = {
     '12': {'UNIFORME': 1459.10, 'EPI': 3315.28, 'FERRAMENTAS': 1633.50, 'MATERIAL APLICADO': 3448.50,
-              'MATERIAL CONSUMO': 889.35, 'LOCAÇÃO MÁQUINAS': 381.15, 'DESPESA INFORMÁTICA': 80.00,
-              'DESPESA TELEFONES CELULARES': 419.35, 'SERVIÇOS CONTRATADOS': 1089.00,
-              'MATERIAIS E PEÇAS DE REPOSIÇÃO EQUIP.': 290.40,
-              'MANUTENÇÃO DE VEÍCULOS': 108.90, 'BENS DE VALORES IRRELEVANTES': 308.55}, 
+           'MATERIAL CONSUMO': 889.35, 'LOCAÇÃO MÁQUINAS': 381.15, 'DESPESA INFORMÁTICA': 80.00,
+           'DESPESA TELEFONES CELULARES': 419.35, 'SERVIÇOS CONTRATADOS': 1089.00,
+           'MATERIAIS E PEÇAS DE REPOSIÇÃO EQUIP.': 290.40,
+           'MANUTENÇÃO DE VEÍCULOS': 108.90, 'BENS DE VALORES IRRELEVANTES': 308.55},
     '31': {'UNIFORME': 5272.79, 'EPI': 4583.69, 'FERRAMENTAS': 1452.00, 'MATERIAL APLICADO': 9801.00,
-              'MATERIAL CONSUMO': 13794.00, 'LOCAÇÃO CARROS': 5262.80, 'LOCAÇÃO GESTÃO ATIVOS': 11980.64,
-              'LOCAÇÃO MÁQUINAS': 7121.47, 'DESPESA INFORMÁTICA': 120.00, 'DESPESA TELEFONES CELULARES': 454.24,
-              'SERVIÇOS CONTRATADOS': 6534.00, 'MATERIAIS E PEÇAS DE REPOSIÇÃO EQUIP.': 7078.50,
-              'MANUTENÇÃO DE VEÍCULOS': 816.75,
-              'CARTÃO COMBUSTÍVEL': 3601.07, 'REFEIÇÕES EXTRAS': 110.00, 'PEDAGIO': 180.00, 'BENS DE VALORES IRRELEVANTES': 290.40,
-              'ESTACIONAMENTO': 20.00},
+           'MATERIAL CONSUMO': 13794.00, 'LOCAÇÃO CARROS': 5262.80, 'LOCAÇÃO GESTÃO ATIVOS': 11980.64,
+           'LOCAÇÃO MÁQUINAS': 7121.47, 'DESPESA INFORMÁTICA': 120.00, 'DESPESA TELEFONES CELULARES': 454.24,
+           'SERVIÇOS CONTRATADOS': 6534.00, 'MATERIAIS E PEÇAS DE REPOSIÇÃO EQUIP.': 7078.50,
+           'MANUTENÇÃO DE VEÍCULOS': 816.75,
+           'CARTÃO COMBUSTÍVEL': 3601.07, 'REFEIÇÕES EXTRAS': 110.00, 'PEDAGIO': 180.00,
+           'BENS DE VALORES IRRELEVANTES': 290.40,
+           'ESTACIONAMENTO': 20.00},
     '26': {'UNIFORME': 2968.74, 'EPI': 2546.63, 'FERRAMENTAS': 889.35, 'MATERIAL APLICADO': 5000.00,
-              'MATERIAL CONSUMO': 5000.00, 'LOCAÇÃO MÁQUINAS': 10890.00, 'DESPESA INFORMÁTICA': 320.00,
-              'DESPESA TELEFONES CELULARES': 429.27, 'SERVIÇOS CONTRATADOS': 889.35,
-              'MATERIAIS E PEÇAS DE REPOSIÇÃO EQUIP.': 2286.90,
-              'MANUTENÇÃO DE VEÍCULOS': 108.90, 'CARTÃO COMBUSTÍVEL': 2281.90, 'PEDAGIO': 680.00, 'BENS DE VALORES IRRELEVANTES': 1089.00,
-              'LOCAÇÃO DE CARROS LEVES': 4531.56},
+           'MATERIAL CONSUMO': 5000.00, 'LOCAÇÃO MÁQUINAS': 10890.00, 'DESPESA INFORMÁTICA': 320.00,
+           'DESPESA TELEFONES CELULARES': 429.27, 'SERVIÇOS CONTRATADOS': 889.35,
+           'MATERIAIS E PEÇAS DE REPOSIÇÃO EQUIP.': 2286.90,
+           'MANUTENÇÃO DE VEÍCULOS': 108.90, 'CARTÃO COMBUSTÍVEL': 2281.90, 'PEDAGIO': 680.00,
+           'BENS DE VALORES IRRELEVANTES': 1089.00,
+           'LOCAÇÃO DE CARROS LEVES': 4531.56},
     '20': {'UNIFORME': 930.12, 'EPI': 1329.40, 'FERRAMENTAS': 771.38, 'MATERIAL APLICADO': 95269.68,
-              'MATERIAL CONSUMO': 12514.75, 'LOCAÇÃO MÁQUINAS': 8893.50, 'DESPESA INFORMÁTICA': 120.00,
-              'DESPESA TELEFONES CELULARES': 93.19, 'SERVIÇOS CONTRATADOS': 9801.00,
-              'MATERIAIS E PEÇAS DE REPOSIÇÃO EQUIP.': 4083.75,
-              'MANUTENÇÃO DE VEÍCULOS': 381.15, 'CARTÃO COMBUSTÍVEL': 653.40, 'BENS DE VALORES IRRELEVANTES': 2178.00, 'MANUT. FERRAM. DISPOSITIVOS MOVEIS': 199.65,
-              'LOCAÇÃO DE CARROS LEVES': 1197.90, 'LOCAÇÃO GESTAO DE ATIVOS': 972.33}
+           'MATERIAL CONSUMO': 12514.75, 'LOCAÇÃO MÁQUINAS': 8893.50, 'DESPESA INFORMÁTICA': 120.00,
+           'DESPESA TELEFONES CELULARES': 93.19, 'SERVIÇOS CONTRATADOS': 9801.00,
+           'MATERIAIS E PEÇAS DE REPOSIÇÃO EQUIP.': 4083.75,
+           'MANUTENÇÃO DE VEÍCULOS': 381.15, 'CARTÃO COMBUSTÍVEL': 653.40, 'BENS DE VALORES IRRELEVANTES': 2178.00,
+           'MANUT. FERRAM. DISPOSITIVOS MOVEIS': 199.65,
+           'LOCAÇÃO DE CARROS LEVES': 1197.90, 'LOCAÇÃO GESTAO DE ATIVOS': 972.33}
 }
 
 
@@ -61,7 +64,7 @@ def process_ut_data(ut_key, data, df_excel):
     return df_grouped, orçamento_total, despesas_totais, saldo_final
 
 
-# 1. Adicionar a função de criação de gráficos
+# Função para criar gráfico
 def criar_grafico(df, ut_key):
     fig = go.Figure()
 
@@ -109,7 +112,10 @@ app.layout = dbc.Container([
                     dbc.Row([
                         dbc.Col([
                             html.Legend("Controle de Orçamento", style={'font-size': '20px', 'font-weight': 'bold'}),
+                            html.P("JOHNSON & KENVUE ",
+                                   style={'font-size': '16px', 'font-weight': 'bold'})
                         ], sm=12, md=8),
+
                         dbc.Col([
                             ThemeSwitchAIO(aio_id="theme", themes=[dbc.themes.FLATLY, dbc.themes.DARKLY]),
                         ], sm=12, md=4),
@@ -117,7 +123,7 @@ app.layout = dbc.Container([
                     dbc.Row([
                         dbc.Button("Oracle", href="https://login-euld-saasfaprod1.fa.ocs.oraclecloud.com/",
                                    target="_blank", color="danger")
-                    ], style={'margin-top': '20px'})
+                    ], style={'margin-top': '30px'})
                 ])
             ], style={'height': '100%'})
         ], sm=12, lg=4)
@@ -127,6 +133,13 @@ app.layout = dbc.Container([
     *[
         dbc.Row([
             dbc.Col([
+                # Dropdown de filtro de categoria
+                dcc.Dropdown(
+                    id=f'categoria-dropdown-ut-{ut_key}',
+                    options=[{'label': categoria, 'value': categoria} for categoria in df_excel['CATEGORIA'].unique()],
+                    value=None,  # Valor inicial
+                    placeholder="Selecione uma categoria",
+                ),
                 # Informações de Orçamento
                 dbc.Card([
                     dbc.CardBody([
@@ -136,17 +149,30 @@ app.layout = dbc.Container([
                                 style={"color": "red" if ut_data[ut_key][3] < 0 else "green"}),
                     ], style={'padding': '10px'})
                 ], style={'height': '120px', 'margin-bottom': '10px'}),
-                # Gráfico
+                # Gráfico da UT
                 dbc.Card([
                     dbc.CardBody([
-                        dcc.Graph(figure=figures[ut_key], style={'width': '100%', 'height': '600px'})
+                        dcc.Graph(id=f'graph-ut-{ut_key}', figure=figures[ut_key], style={'width': '100%', 'height': '600px'})
                     ])
                 ]),
             ], width=12),
         ], style={'margin-bottom': '20px'})
-        for ut_key in ut_data.keys()
-    ],
+        for ut_key in data.keys()
+    ]
 ], fluid=True)
+
+
+# Callbacks para atualizar os gráficos ao selecionar uma categoria no dropdown
+for ut_key in data.keys():
+    @app.callback(
+        Output(f'graph-ut-{ut_key}', 'figure'),
+        [Input(f'categoria-dropdown-ut-{ut_key}', 'value')]
+    )
+    def update_graph(categoria_selecionada, ut_key=ut_key):
+        df, _, _, _ = process_ut_data(ut_key, data[ut_key], df_excel)
+        if categoria_selecionada:
+            df = df[df['CATEGORIA'] == categoria_selecionada]  # Filtrar a categoria selecionada
+        return criar_grafico(df, ut_key)
 
 
 # Run the app
