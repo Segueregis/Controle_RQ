51481e6ecf186617fc3043ad766a9cfb0b37c185
Segueregis <105684859+Segueregis@users.noreply.github.com>
2024-11-05 21:41:49 -0300

    Update app2.py

--- app2.py
+++ app2.py
@@ -24,28 +24,41 @@ app2.layout = dbc.Container([
         )
     ]),
     dbc.Row([
-        dbc.Col(dcc.Graph(id="rq-graph"))
+        dbc.Col(dcc.Graph(id="rq-table"))
     ])
 ])
 
-# Callback para atualizar o gráfico com base no Número RQ
+
+# Callback para atualizar a tabela com base no Número RQ
 @app2.callback(
-    Output("rq-graph", "figure"),
+    Output("rq-table", "figure"),
     Input("buscar-rq", "n_clicks"),
     State("rq-input", "value")
 )
-def update_graph(n_clicks, rq_number):
-    if rq_number:
-        df_filtered = df_novembro[df_novembro['REQUISICAO'] == rq_number]
+def update_table(n_clicks, rq_number):
+    if n_clicks > 0 and rq_number:  # Verifica se o botão foi clicado e se há um número de RQ
+        # Filtra em ambos os DataFrames de Outubro e Novembro
+        df_filtered = pd.concat([
+            df_outubro[df_outubro['REQUISICAO'] == rq_number],
+            df_novembro[df_novembro['REQUISICAO'] == rq_number]
+        ])
+
         if not df_filtered.empty:
             fig = go.Figure(data=[go.Table(
-                header=dict(values=list(df_filtered.columns)),
-                cells=dict(values=[df_filtered[col] for col in df_filtered.columns])
+                columnwidth=[200 for _ in df_filtered.columns],  # Largura padrão de 200 pixels por coluna
+                header=dict(values=list(df_filtered.columns), align="left"),
+                cells=dict(values=[df_filtered[col].tolist() for col in df_filtered.columns], align="left")
             )])
         else:
-            fig = go.Figure(data=[go.Table(header=dict(values=["Nenhum dado encontrado"]))])
+            fig = go.Figure(data=[go.Table(
+                header=dict(values=["Nenhum dado encontrado"]),
+                cells=dict(values=[["Verifique o Número RQ inserido"]])
+            )])
         return fig
+
+    # Retorna uma tabela em branco se nada for filtrado
     return go.Figure()
+
 
 if __name__ == "__main__":
     app2.run_server(debug=True)
