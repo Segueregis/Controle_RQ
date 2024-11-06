2160bb66edde6662a51e564079f9bb524e25e228
Segueregis <105684859+Segueregis@users.noreply.github.com>
2024-11-05 22:23:27 -0300

    ATUALIZAÇÃO MENU

--- app.py
+++ app.py
@@ -157,8 +157,8 @@ hamburger_menu = html.Div(
                 ),
             ],
             id="offcanvas",
-            title="Opções",
             is_open=False,
+            placement="start"
         ),
     ]
 )
@@ -285,8 +285,8 @@ def update_graphs(selected_month):
     Input("open-offcanvas", "n_clicks"),
     State("offcanvas", "is_open")
 )
-def toggle_offcanvas(n_clicks, is_open):
-    if n_clicks:
+def toggle_offcanvas(n1, is_open):
+    if n1:
         return not is_open
     return is_open
 
